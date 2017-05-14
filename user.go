42fcb0171c743eb625db8705a1f0d04899d700b5
Greg Ellis <gellis@arcadia.edu>
2017-05-13 22:19:42 -0400

    Updated User model to reflect database additions

--- user.go
+++ user.go
@@ -1,7 +1,29 @@
 // Package models contains weaver-ci model structs
 package models
 
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
 // User model
 type User struct {
+	UserID       uuid.UUID
 	EmailAddress string
+	LastLogin    *time.Time
+	ModifiedOn   *time.Time
+	CreatedOn    *time.Time
+}
+
+// GithubLogin model
+type GithubLogin struct {
+	UserID     uuid.UUID
+	ResetToken string
+}
+
+// AssemblaLogin model
+type AssemblaLogin struct {
+	UserID     uuid.UUID
+	ResetToken string
 }
